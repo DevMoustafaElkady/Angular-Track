613fa58743dc717054baa03ff08853ab13a1b6af
Moustafa Elkady <moustafa.elkady.dev@gmail.com>
2022-04-06 22:25:10 +0200

    enhance structure

--- flights/src/app/app.module.ts
+++ flights/src/app/app.module.ts
@@ -1,9 +1,10 @@
+import { SharedModule } from './Modules/shared/shared.module';
+import { HomeModule } from './Modules/home/home.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { SharedModule } from './Modules/shared/shared.module';
 import { ContactUsComponent } from './views/contact-us/contact-us.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AboutComponent } from './views/about/about.component';
@@ -13,12 +14,12 @@ import { AboutComponent } from './views/about/about.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-   ],
+    SharedModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
